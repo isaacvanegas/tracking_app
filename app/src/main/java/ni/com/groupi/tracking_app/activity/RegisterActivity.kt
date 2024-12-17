c74d1344be453f2f156f4c57364866cf8675c24a
Aburtoz <42686599+Aburtoz@users.noreply.github.com>
2024-12-16 20:49:29 -0600

    feat agregar register

---
+++ app/src/main/java/ni/com/groupi/tracking_app/activity/RegisterActivity.kt
@@ -0,0 +1,23 @@
+package ni.com.groupi.tracking_app.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ni.com.groupi.myapplication.R
+import ni.com.groupi.myapplication.databinding.ActivityLoginBinding
+import ni.com.groupi.myapplication.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
